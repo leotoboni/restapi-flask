94f240dbccd24cfa26b46d159a8504d3cfd76daf
Leonardo Otoboni <leonardo.otoboni@protonmail.com>
2023-01-05 10:54:49 -0300

    fix: formatting according to flake8

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+
 class Users(Resource):
     def get(self):
         return {"message": "user 1"}
+
 
 class User(Resource):
     def get(self):
